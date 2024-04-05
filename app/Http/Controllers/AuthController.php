3f785f68b52876a298d36def3d58e2f2e8a1e623
Renan Caldas <renan@renancaldas.dev.br>
2024-04-05 20:57:12 -0300

    refactor: refatorado metodos de login, registe e logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,14 +9,19 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
 
 class AuthController extends Controller
 {
     public function register(AuthRequest $request): JsonResponse
     {
         try {
-            $user = User::create($request->validated());
+            $userData = $request->validated();
+
+            $userData['password'] = Hash::make($userData['password']);
+            $user = User::create($userData);
 
             $token = $user->createToken('auth_token')->plainTextToken;
 
@@ -24,33 +29,35 @@ class AuthController extends Controller
                 'message' => 'Usuário criado com sucesso!',
                 'access_token' => $token
             ], Response::HTTP_CREATED);
-        } catch (ValidationException $e) {
-            $errors = $e->validator->errors()->toArray();
-            return response()->json(['error' => $errors], Response::HTTP_NOT_FOUND);
+        } catch (\Exception $e) {
+            Log::error("Erro ao registrar usuário: " . $e->getMessage());
+            return response()->json(['error' => 'Erro ao registrar usuário.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
     public function login(LoginRequest $request): JsonResponse
     {
         try {
+
             $credentials = $request->only('email', 'password');
 
-            if (Auth::attempt($credentials)) {
-                $user = Auth::user();
-                $token = $user->createToken('auth_token')->plainTextToken;
-
-                return response()->json(['access_token' => $token, 'message' => 'Seja Bem vindo, ' . $user->name . '!!'], Response::HTTP_OK);
-            } else {
-                return response()->json(['message' => 'Não autorizado, email ou senha incorretos.'], Response::HTTP_UNAUTHORIZED);
+            if (!Auth::attempt($credentials)) {
+                return response()->json(['message' => 'Não autorizado, email ou senha incorreto.'], Response::HTTP_UNAUTHORIZED);
             }
-        } catch (ValidationException $e) {
-            return response()->json([
-                'message' => $e->getMessage()
-            ], Response::HTTP_NOT_FOUND);
+
+            $user = User::find(Auth::id());
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json(['access_token' => $token, 'message' => 'Seja Bem vindo, ' . $user->name . '!!'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('erro ao fazer login: ' . $e->getMessage());
+
+            return response()->json(['error' => 'Erro ao fazer login.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
         try {
             $request->user()->tokens()->delete();
@@ -58,9 +65,8 @@ class AuthController extends Controller
                 'message' => 'Logout bem-sucedido'
             ], Response::HTTP_OK);
         } catch (\Exception $e) {
-            return response()->json([
-                'message' => $e->getMessage()
-            ], Response::HTTP_NOT_FOUND);
+            Log::error('Erro ao fazer logout: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao fazer logout.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
