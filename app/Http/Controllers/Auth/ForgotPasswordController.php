171887e0749580ea50d82e56253113dd66ec0758
Renan Caldas <renan@renancaldas.dev.br>
2024-04-05 20:57:27 -0300

    refactor: refatorado metodo de recuperar senha

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,20 +4,31 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
-    public function forgotPassword(Request $request)
+    public function forgotPassword(Request $request): JsonResponse
     {
-        $request->validate(['email' => 'required|email']);
+        try {
+            $request->validate(['email' => 'required|email']);
 
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
 
-        return $status === Password::RESET_LINK_SENT
-            ? response()->json(['Message' => 'E-mail eviando com sucesso!'], 200)
-            : response()->json(['Message' => 'Não foi possível enviar o e-mail'], 400);
+            if ($status === Password::RESET_LINK_SENT) {
+                return response()->json(['message' => 'E-mail enviado com sucesso!'], Response::HTTP_OK);
+            } else {
+                return response()->json(['message' => 'Não foi possível enviar o e-mail'], Response::HTTP_BAD_REQUEST);
+            }
+        } catch (\Exception $e) {
+            Log::error('Erro ao enviar e-mail de redefinição de senha: ' . $e->getMessage());
+
+            return response()->json(['error' => 'Erro ao enviar e-mail de redefinição de senha.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
