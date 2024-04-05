3ef5ab6b713205ba33fc7a81823d910f6a346f84
Renan Caldas <renan@renancaldas.dev.br>
2024-04-04 23:12:51 -0300

    feat: criado função de recuperação de senha

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json(['Message' => 'E-mail eviando com sucesso!'], 200)
+            : response()->json(['Message' => 'Não foi possível enviar o e-mail'], 400);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\GoalController;
@@ -27,6 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/forgotpassword', [ForgotPasswordController::class, 'forgotPassword'])->name('password.reset');
 
 Route::group([
     'prefix' => 'users',
