349d2c923a1ffd64a622bc3089bdbb36ae423ebd
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 16:11:39 -0300

    feat: refactor forgotpassword

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,19 +6,33 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
     public function forgotPassword(Request $request): JsonResponse
     {
         try {
-            $request->validate(['email' => 'required|email']);
+            $validator = Validator::make($request->all(), [
+                'email_remember' => 'required|email',
+            ], [
+                'email_remember.required' => 'O campo de email é obrigatório.',
+                'email_remember.email' => 'O campo de email deve ser um endereço de e-mail válido.',
+            ]);
 
-            $status = Password::sendResetLink(
-                $request->only('email')
-            );
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $user = DB::table('users')->where('email', $request->email_remember)->first();
+            if (!$user) {
+                return response()->json(['message' => 'E-mail não encontrado'], Response::HTTP_NOT_FOUND);
+            }
+
+            $status = Password::sendResetLink(['email' => $request->email_remember]);
 
             if ($status === Password::RESET_LINK_SENT) {
                 return response()->json(['message' => 'E-mail enviado com sucesso!'], Response::HTTP_OK);
